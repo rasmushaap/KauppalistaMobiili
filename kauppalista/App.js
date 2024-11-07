8f6f0c8485a18c28d16466fa48772c0e930858aa
rasmushaap <rasmus.haapalainen@gmail.com>
2024-11-07 15:24:34 +0200

    Projektin luonti ja etusivun otsikon muutos

---
+++ kauppalista/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Kauppalista</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "black",
+  },
+});
